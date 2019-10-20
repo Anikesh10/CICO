4f3dacfb9d5b4d2862384ff1a64af7023a9e6524
Anikesh10 <anikeshrajan@gmail.com>
2019-10-21 03:06:08 +0530

    add loader screen

--- CICO/src/screens/loader/index.js
+++ CICO/src/screens/loader/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import {Text} from 'react-native';
+import styled, {withTheme} from 'styled-components/native';
+import {ActivityIndicator} from 'react-native-paper';
 
-const Loader = () => {
-  return <Text>Loader</Text>;
+const Loader = props => {
+  return (
+    <Wrapper>
+      <ActivityIndicator
+        color={props.theme.APP_COLOR.PRIMARY.TITLE}
+        size="large"
+      />
+    </Wrapper>
+  );
 };
 
-export default Loader;
+const Wrapper = styled.View`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 10;
+  background-color: ${props => props.theme.APP_COLOR.PRIMARY.MAIN};
+  opacity: 0.8;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default withTheme(Loader);

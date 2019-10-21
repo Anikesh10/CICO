e5b1b011380507aedb15788cc97cafa79e98b7ad
Anikesh10 <anikeshrajan@gmail.com>
2019-10-21 09:55:13 +0530

    remove env file

--- CICO/src/screens/login/duck/service.js
+++ CICO/src/screens/login/duck/service.js
@@ -2,7 +2,7 @@
 import FetchUtils from '../../../utils/FetchUtils';
 
 const authenticateUser = async body => {
-  const url = '/user/authenticate';
+  const url = 'user/authenticate';
   try {
     let response = await FetchUtils.postData(url, body, 'Login user ==>');
     return response;
